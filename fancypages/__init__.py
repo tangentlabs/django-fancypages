5b283e1dd48b811b54345de53c177d78e4eb084a
Sebastian Vetter <sebastian@roadside-developer.com>
2013-07-15 11:46:06 +1000

    Bring path function in line with oscar fancypages

--- fancypages/__init__.py
+++ fancypages/__init__.py
@@ -2,6 +2,6 @@ import os
 
 __version__ = (0, 0, 1, 'alpha', 1)
 
-FP_MAIN_TEMPLATE_DIR = os.path.join(
-    os.path.dirname(os.path.abspath(__file__))
-)
+
+def get_fancypages_paths(path):
+    return [os.path.join(os.path.dirname(os.path.abspath(__file__)), path)]
