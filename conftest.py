b64cac49cbb352b3ab4cd73b620136681c92f066
Sebastian Vetter <sebastian@roadside-developer.com>
2013-07-26 16:40:16 +1000

    Switch from nose to pytest

---
+++ conftest.py
@@ -0,0 +1,85 @@
+import os
+
+from django.conf import settings
+
+import fancypages as fp
+
+location = lambda x: os.path.join(os.path.dirname(os.path.realpath(__file__)), x)
+sandbox = lambda x: location("sandbox/%s" % x)
+
+
+def pytest_configure():
+    settings.configure(
+        DATABASES={
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': ':memory:',
+            }
+        },
+        MEDIA_ROOT=sandbox('public/media'),
+        MEDIA_URL='/media/',
+        STATIC_URL='/static/',
+        STATICFILES_DIRS=(sandbox('static/'),),
+        STATIC_ROOT=sandbox('public'),
+        STATICFILES_FINDERS=(
+            'django.contrib.staticfiles.finders.FileSystemFinder',
+            'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+            'compressor.finders.CompressorFinder',
+        ),
+        TEMPLATE_LOADERS=(
+            'django.template.loaders.filesystem.Loader',
+            'django.template.loaders.app_directories.Loader',
+        ),
+        TEMPLATE_CONTEXT_PROCESSORS = (
+            "django.contrib.auth.context_processors.auth",
+            "django.core.context_processors.request",
+            "django.core.context_processors.debug",
+            "django.core.context_processors.i18n",
+            "django.core.context_processors.media",
+            "django.core.context_processors.static",
+            "django.contrib.messages.context_processors.messages",
+        ),
+        MIDDLEWARE_CLASSES=(
+            'django.middleware.common.CommonMiddleware',
+            'django.contrib.sessions.middleware.SessionMiddleware',
+            'django.middleware.csrf.CsrfViewMiddleware',
+            'django.contrib.auth.middleware.AuthenticationMiddleware',
+            'django.contrib.messages.middleware.MessageMiddleware',
+            'debug_toolbar.middleware.DebugToolbarMiddleware',
+            'fancypages.middleware.EditorMiddleware',
+        ),
+        ROOT_URLCONF='sandbox.sandbox.urls',
+        TEMPLATE_DIRS=(
+            sandbox('templates'),
+        ),
+        INSTALLED_APPS=(
+            'django.contrib.auth',
+            'django.contrib.contenttypes',
+            'django.contrib.sessions',
+            'django.contrib.sites',
+            'django.contrib.messages',
+            'django.contrib.staticfiles',
+            'django.contrib.admin',
+
+            'model_utils',
+            'compressor',
+            'twitter_tag',
+            'sorl.thumbnail',
+            'rest_framework',
+        ) + fp.get_fancypages_apps(),
+        AUTHENTICATION_BACKENDS=(
+            'django.contrib.auth.backends.ModelBackend',
+        ),
+        LOGIN_REDIRECT_URL='/accounts/',
+        APPEND_SLASH=True,
+        NOSE_ARGS=[
+            '-s',
+            '--with-specplugin',
+        ],
+        # required for django-twitter-tag throws an exception if not
+        # in the settings
+        TWITTER_OAUTH_TOKEN='',
+        TWITTER_OAUTH_SECRET='',
+        TWITTER_CONSUMER_KEY='',
+        TWITTER_CONSUMER_SECRET='',
+    )
