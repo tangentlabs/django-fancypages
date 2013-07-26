126c4f94ceb73c14a1cb45942d0e42bde1b1cc4e
Sebastian Vetter <sebastian@roadside-developer.com>
2013-07-26 16:16:21 +1000

    Add factory boy for testing and start testing of block rendering

---
+++ tests/unit/blocks/test_text_block.py
@@ -0,0 +1,28 @@
+import mock
+
+from django.test import TestCase
+from django.template import RequestContext
+
+from fancypages.test import factories
+
+
+class TestTextBlock(TestCase):
+
+    def setUp(self):
+        super(TestTextBlock, self).setUp()
+        self.user = factories.UserFactory.build()
+
+        self.request_context = RequestContext(mock.MagicMock())
+        self.request_context['user'] = self.user
+
+    def test_can_be_rendered_in_template(self):
+        block = factories.TextBlockFactory.build()
+        block.id = 666
+        renderer = block.get_renderer_class()(block, self.request_context)
+        block_html = renderer.render()
+
+        self.assertIn(
+            "block-{0}-{1}".format(block.id, block.code),
+            block_html,
+        )
+        self.assertIn(block.text, block_html)
