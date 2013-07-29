b8906e596193bcdc22d5cdd6b4ce57347e262621
Sebastian Vetter <sebastian@roadside-developer.com>
2013-07-29 15:19:04 +1000

    Clean up default FP settings

--- fancypages/defaults.py
+++ fancypages/defaults.py
@@ -1,35 +1,7 @@
-########## INSTALLED APPS
-FANCYPAGES_REQUIRED_APPS = (
-    'rest_framework',
-    'model_utils',
-    'south',
-    'compressor',
-    'twitter_tag',
-    'sorl.thumbnail',
-)
-FANCYPAGES_APPS = (
-    'fancypages',
-    'fancypages.api',
-    'fancypages.assets',
-    'fancypages.dashboard',
-)
-########## END INSTALLED APPS
-
-########## COMPRESSOR SETTINGS
-# Compressor and pre-compiler settings for django-compressor
-COMPRESS_ENABLED = True
-COMPRESS_OFFLINE = False
-COMPRESS_OUTPUT_DIR = 'cache'
-
-COMPRESS_PRECOMPILERS = (
-    ('text/less', 'lessc {infile} {outfile}'),
-)
-
-COMPRESS_JS_FILTERS = [
-    'compressor.filters.jsmin.JSMinFilter',
-    'compressor.filters.template.TemplateFilter',
-]
-########## END COMPRESSOR SETTINGS
+########## FANCYPAGES SETTINGS
+FP_HOMEPAGE_NAME = 'Home'
+FP_DEFAULT_TEMPLATE = 'fancypages/pages/page.html'
+########## END FANCYPAGES SETTINGS
 
 ########## TWITTER TAG SETTINGS
 TWITTER_OAUTH_TOKEN = ''
