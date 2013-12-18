2423826639ec62bb3cf4a7c91aabbeed0370a58b
Sebastian Vetter <sebastian@roadside-developer.com>
2013-12-18 16:29:05 +1100

    Add mixin to generate language-specific templates

--- fancypages/middleware.py
+++ fancypages/middleware.py
@@ -14,9 +14,8 @@ def replace_insensitive(string, target, replacement):
     """
     Similar to string.replace() but is case insensitive
     Code borrowed from:
-    http://forums.devshed.com/python-programming-11/
-            case-insensitive-string-replace-490921.html
-    """
+    http://forums.devshed.com/python-programming-11/case-insensitive-string-replace-490921.html
+    """ # NOQA
     no_case = string.lower()
     index = no_case.rfind(target.lower())
     if index >= 0:
