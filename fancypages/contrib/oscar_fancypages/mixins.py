1709c602b8a423d1eee6521c5e74987db0fc8b81
Sebastian Vetter <sebastian@roadside-developer.com>
2014-01-28 12:37:00 +1100

    Change context object for product list view in Oscar contrib

--- fancypages/contrib/oscar_fancypages/mixins.py
+++ fancypages/contrib/oscar_fancypages/mixins.py
@@ -4,11 +4,11 @@ from ... import mixins
 class OscarFancyPageMixin(mixins.FancyPageMixin):
     node_attr_name = 'category'
     slug_url_kwarg = 'category_slug'
-    context_object_name = 'fancypage'
+    context_object_name = 'products'
 
     def get_context_data(self, **kwargs):
         ctx = super(OscarFancyPageMixin, self).get_context_data(**kwargs)
-        ctx[self.context_object_name] = getattr(self, self.page_attr_name)
+        ctx['fancypage'] = getattr(self, self.page_attr_name)
         if self.category:
             ctx['object'] = self.category
             for container in self.category.page.containers.all():
