6290d72458a470947c5da651968cf20f8714c646
Sebastian Vetter <sebastian@roadside-developer.com>
2014-02-12 15:02:43 +1100

    Fix class loader called in Oscar contrib package

--- fancypages/contrib/oscar_fancypages/views.py
+++ fancypages/contrib/oscar_fancypages/views.py
@@ -1,9 +1,9 @@
-from oscar.core.loading import load_class
+from oscar.core.loading import get_class
 
 from . import mixins
 
 
-ProductCategoryView = load_class('catalogue.views', 'ProductCategoryView')
+ProductCategoryView = get_class('catalogue.views', 'ProductCategoryView')
 
 
 class FancyPageDetailView(mixins.OscarFancyPageMixin, ProductCategoryView):
