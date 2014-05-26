a018cbf801be237417c5cf432a31b291e3f34f02
Sebastian Vetter <sebastian@roadside-developer.com>
2014-05-26 17:01:42 +1000

    Add grunt task for building CSS files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,49 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    var staticPath = function (path) {
+        return 'fancypages/static/fancypages/' + path;
+    };
 
-    mocha: {
-        test: {                                                                                                                                                                                                                                                           
-            options: {                                                                                                                                                                                                                                                    
-                reporter: 'Spec'                                                                                                                                                                                                                                          
-            },                                                                                                                                                                                                                                                            
-            src: ['tests/javascript/**/*.html'],                                                                                                                                                                                                                          
-        },                                                                                                                                                                                                                                                                
-    }
-  });
+    var lessFiles = function (files) {
+        var mapping = {};
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-mocha');
+        for (var idx in files) {
+            mapping[file[idx][0]] = file[idx][1];
+        };
+        return mapping;
+    };
 
-  // Default task(s).
-  grunt.registerTask('test', ['mocha']);
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
+        less: {
+            dev: {
+                options: {
+                    sourceMap: true,
+                },
+                files: {
+                    "fancypages/static/fancypages/dist/css/assets.css": "fancypages/static/fancypages/src/less/assets.less",
+                    "fancypages/static/fancypages/dist/css/fancypages.css": "fancypages/static/fancypages/src/less/fancypages.less",
+                    "fancypages/static/fancypages/dist/css/page.css": "fancypages/static/fancypages/src/less/page.less",
+                    "fancypages/static/fancypages/dist/css/page-management.css": "fancypages/static/fancypages/src/less/page-management.less",
+                }
+            }
+        },
+
+        mocha: {
+            test: {
+                options: {
+                    reporter: 'Spec'
+                },
+                src: ['tests/javascript/**/*.html'],
+            },
+        }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-mocha');
+    grunt.loadNpmTasks('grunt-contrib-less');
+
+    grunt.registerTask('test', ['mocha']);
+    grunt.registerTask('default', ['less:dev']);
 };
