3183428963088d532a56933a465dc48a19c010c6
Sebastian Vetter <sebastian@roadside-developer.com>
2014-06-19 15:17:26 +1000

    Fix user login/logout in standalone sandbox

--- sandboxes/fancypages/sandbox/urls.py
+++ sandboxes/fancypages/sandbox/urls.py
@@ -21,6 +21,10 @@ urlpatterns = patterns(
 
     url(r'^admin/', include(admin.site.urls)),
 
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login', name='login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout',
+        name='logout'),
+
     url(r'^', include(fancypages.urls)),
 )
 
