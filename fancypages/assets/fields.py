486633791bea00c6a846b88124860efbc7532433
Sebastian Vetter <sebastian@roadsi.de>
2014-07-16 11:52:17 +1000

    Fix south introspection rule for custom AssetField

--- fancypages/assets/fields.py
+++ fancypages/assets/fields.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, absolute_import
+import django
 from django.db.models.fields.related import ForeignKey
 
 from .forms import AssetField
@@ -16,5 +19,7 @@ class AssetKey(ForeignKey):
         return [asset_obj.id, asset_obj._meta.module_name]
 
 
-from south.modelsinspector import add_introspection_rules
-add_introspection_rules([], ["^fancypages\.assets\.fields\.AssetKey"])
+# This is only required for Django version < 1.7 suing South for migrations
+if django.VERSION[:2] < (1, 7):
+    from south.modelsinspector import add_introspection_rules
+    add_introspection_rules([], ["^fancypages\.assets\.fields\.AssetKey"])
